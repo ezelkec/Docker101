a96083fdede3e75f738abb5649457d84df5fb018
ezel_kec <ezel_kec@hotmail.com>
2016-08-20 00:12:52 +0000

    project2 update

--- fetcher/task4/sample.sh
+++ fetcher/task4/sample.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget https://www.dropbox.com/download?plat=lnx.x86_64
-tar -xf /tmp/dropbox.tar.gz 
+wget -O /tmp/dropbox.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64
+tar -xf /tmp/downloads/dropbox.tar.gz 
